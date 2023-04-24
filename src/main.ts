d16eaa8546957a798653f5e63a130ae209cd2995
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2023-04-24 21:34:50 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:{origin:"*"}});
   app.enableCors({
-    origin:["*"],
+    origin:["*","https://diploma-food-app.vercel.app/","http://localhost:3000/"],
     methods: ['GET','HEAD','PUT','PATCH','POST','DELETE', 'OPTIONS'],
 
     //credentials:true
